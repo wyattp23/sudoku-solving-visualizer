6460dbae951fbc8d36e3fe2c87db51023ab2af7d
Wyatt Pierce <wyattp23@gmail.com>
2021-07-03 15:02:23 -0700

    Add board values state.

--- sudoku-solver-visualized/src/components/SudokuSolver.js
+++ sudoku-solver-visualized/src/components/SudokuSolver.js
@@ -1,12 +1,29 @@
 import Board from './Board.js'
+import { useState } from 'react'
 
 const SudokuSolver = () => {
+    const [boardValues, setBoardValues] = useState(defaultBoardValues);
+
     return (
-        <Board values={boardValues}/>
+        <div>
+            <button onClick={() => setBoardValues(incrementBoard(boardValues))}>add</button>
+            <Board values={boardValues}/>
+        </div>
+
     );
 }
 
-const boardValues = [
+const incrementBoard = (boardValues) => {
+    return (
+        boardValues.map((values) => (
+            values.map((value => (
+                value + 1
+            )))
+        ))
+    )
+}
+
+const defaultBoardValues = [
     [1, 0, 3, 0, 5, 0, 7, 0, 9],
     [11, 22, 33, 4, 5, 6, 7, 8, 9],
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
