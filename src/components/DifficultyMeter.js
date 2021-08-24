90511bc61376519a0822cc53a17d5680081a8866
Wyatt Pierce <wyattp23@gmail.com>
2021-08-24 16:09:13 -0700

    Remove unused difficulty variable.

--- src/components/DifficultyMeter.js
+++ src/components/DifficultyMeter.js
@@ -1,10 +1,8 @@
-import { boardDifficulties } from "../unsolvedBoards";
 import star from "../star.svg"
 
 export const DifficultyMeter = (props) => {
     return (
         <div id="difficultyMeter">
-            {/* <p id="difficulty">{boardDifficulties[props.currentBoardIdx]}</p> */}
             {[...Array(props.currentBoardIdx + 1)].map((e, i) => <img key={i} src={star} alt="star" className="star"/>)}
         </div>
     )
