47a2cdbf51b5152b85267339d51b3f4afbc5714f
Wyatt Pierce <wyattp23@gmail.com>
2021-08-29 16:47:46 -0700

    Adjust slider spacing.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 export const Slider = () => {
+  const [maxDelay] = useState(25);
+  const [minDelay] = useState(1);
+  const [delay, setDelay] = useState(maxDelay);
+
   return (
+    <>
+    <p>Delay: {delay}ms</p>
     <div class="slidercontainer">
-      <input type="range" min="1" max="25" id="animSpeed" />
+      <input
+        type="range" 
+        min={minDelay}
+        max={maxDelay} 
+        id="animSpeed"
+        onChange={(e) => setDelay(e.target.value)} />
     </div>
+    </>
   );
 };
