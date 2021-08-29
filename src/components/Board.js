7b70c9902852bf4fb15bdfb784a9239628a5f0d6
Wyatt Pierce <wyattp23@gmail.com>
2021-08-29 14:42:22 -0700

    Convert board to table.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,29 @@
 import { Cell } from "./Cell.js";
 
+// export const Board = (props) => {
+//   return (
+//     <div id="board">
+//       {props.board.map((row, i) => (
+//         <div className="row">
+//           {row.map((cell, j) => (
+//             <Cell 
+//               key={cell.id} 
+//               number={cell.number}
+//               status={cell.status}
+//               id={`cell[${i}][${j}]`}
+//             />
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
 export const Board = (props) => {
   return (
-    <div id="board">
+    <table id="board">
       {props.board.map((row, i) => (
-        <div className="row">
+        <tr className="row">
           {row.map((cell, j) => (
             <Cell 
               key={cell.id} 
@@ -13,8 +32,8 @@ export const Board = (props) => {
               id={`cell[${i}][${j}]`}
             />
           ))}
-        </div>
+        </tr>
       ))}
-    </div>
+    </table>
   );
 };
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,19 +1,19 @@
 export const Cell = (props) => {
   return (
-    <div className={`cell ${props.status} ${getCellClass(props.id)}`} id={props.id}>
+    <td className={`cell ${props.status}`} id={props.id}>
       {props.number ? props.number : ""}
-    </div>
+    </td>
   );
 };
 
-const getCellClass = (id) => {
-  var [row, col] = id.match(/\d/g);
-  var classes = [];
+// const getCellClass = (id) => {
+//   var [row, col] = id.match(/\d/g);
+//   var classes = [];
 
-  if (row == "0" || row == "3" || row == "6") classes.push("cell-top-border");
-  if (col == "0" || col == "3" || col == "6") classes.push("cell-left-border");
-  if (row == "8") classes.push("cell-bottom-border");
-  if (col == "8") classes.push("cell-right-border");
+//   if (row == "0" || row == "3" || row == "6") classes.push("cell-top-border");
+//   if (col == "0" || col == "3" || col == "6") classes.push("cell-left-border");
+//   if (row == "8") classes.push("cell-bottom-border");
+//   if (col == "8") classes.push("cell-right-border");
 
-  return classes.join(" ");
-};
+//   return classes.join(" ");
+// };
