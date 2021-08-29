8ef4ac2aa383e7f1977ddab642d014e3b447bbea
Wyatt Pierce <wyattp23@gmail.com>
2021-08-29 14:43:53 -0700

    Remove old implementation.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,5 @@
 import { Cell } from "./Cell.js";
 
-// export const Board = (props) => {
-//   return (
-//     <div id="board">
-//       {props.board.map((row, i) => (
-//         <div className="row">
-//           {row.map((cell, j) => (
-//             <Cell 
-//               key={cell.id} 
-//               number={cell.number}
-//               status={cell.status}
-//               id={`cell[${i}][${j}]`}
-//             />
-//           ))}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
 export const Board = (props) => {
   return (
     <table id="board">
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,15 +5,3 @@ export const Cell = (props) => {
     </td>
   );
 };
-
-// const getCellClass = (id) => {
-//   var [row, col] = id.match(/\d/g);
-//   var classes = [];
-
-//   if (row == "0" || row == "3" || row == "6") classes.push("cell-top-border");
-//   if (col == "0" || col == "3" || col == "6") classes.push("cell-left-border");
-//   if (row == "8") classes.push("cell-bottom-border");
-//   if (col == "8") classes.push("cell-right-border");
-
-//   return classes.join(" ");
-// };
