c35fe88261d9237523e464bbb98b293f0f6e0a04
Wyatt Pierce <wyattp23@gmail.com>
2021-08-31 15:15:24 -0700

    Add animation to stars on highest difficulty.

--- src/components/DifficultyMeter.js
+++ src/components/DifficultyMeter.js
@@ -3,7 +3,14 @@ import star from "../star.svg"
 export const DifficultyMeter = (props) => {
     return (
         <div id="difficultyMeter">
-            {[...Array(props.currentBoardIdx + 1)].map((e, i) => <img key={i} src={star} alt="star" className="star"/>)}
+            {[...Array(props.currentBoardIdx + 1)].map((e, i) => 
+                <img 
+                    key={i} 
+                    src={star} 
+                    alt="star" 
+                    className={`star${props.currentBoardIdx == 3 ? " extreme" : ""}`}
+                />
+            )}
         </div>
     )
 }
