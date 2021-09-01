0053757d66edf2339975bc0ba5a6a620b9402030
Wyatt Pierce <wyattp23@gmail.com>
2021-09-01 13:51:41 -0700

    Add tbody to board.

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,18 +3,20 @@ import { Cell } from "./Cell.js";
 export const Board = (props) => {
   return (
     <table id="board">
-      {props.board.map((row, i) => (
-        <tr className="row">
-          {row.map((cell, j) => (
-            <Cell 
-              key={cell.id} 
-              number={cell.number}
-              status={cell.status}
-              id={`cell[${i}][${j}]`}
-            />
-          ))}
-        </tr>
-      ))}
+      <tbody>
+        {props.board.map((row, i) => (
+          <tr className="row">
+            {row.map((cell, j) => (
+              <Cell 
+                key={cell.id} 
+                number={cell.number}
+                status={cell.status}
+                id={`cell[${i}][${j}]`}
+              />
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
