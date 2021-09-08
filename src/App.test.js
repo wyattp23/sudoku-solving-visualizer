ddc9f5482ec30119ba25a53244ba16a9998f1282
Wyatt Pierce <wyattp23@gmail.com>
2021-09-07 18:28:14 -0700

    Add render test for solver button.

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test("renders header", () => {
   const headerElement = screen.getByText(/Sudoku Solving Visualizer/i);
   expect(headerElement).toBeInTheDocument();
 });
+
+test("renders solve button", () => {
+  render(<App />);
+  const solveButton = screen.getByText(/solve/i);
+  expect(solveButton).toBeInTheDocument();
+});
