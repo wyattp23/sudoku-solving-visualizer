57c8eb41328cb40296f855600949d3982492c750
Wyatt Pierce <wyattp23@gmail.com>
2021-09-08 21:21:17 -0700

    Remove render tests.

--- src/App.test.js
+++ src/App.test.js
@@ -3,30 +3,6 @@ import App from "./App";
 import { solve } from "./algorithms/solve.js";
 import { unsolvedBoards, solutions } from "./unsolvedBoards.js";
 
-test("renders header", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Sudoku Solving Visualizer/i);
-  expect(headerElement).toBeInTheDocument();
-});
-
-test("renders solve button", () => {
-  render(<App />);
-  const solveButton = screen.getByText(/solve/i);
-  expect(solveButton).toBeInTheDocument();
-});
-
-test("renders reset button", () => {
-  render(<App />);
-  const resetButton = screen.getByText(/reset/i);
-  expect(resetButton).toBeInTheDocument();
-});
-
-test("renders new board button", () => {
-  render(<App />);
-  const newBoardButton = screen.getByText(/new board/i);
-  expect(newBoardButton).toBeInTheDocument();
-});
-
 test("returns correct solution to unsolved board", () => {
   for (let i = 0; i < solutions.length; i++) {
     let [ ,board] = solve(unsolvedBoards[i]);
